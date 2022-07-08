5068645cc4b91bbe2140f5edb01d471a56675743
Fede Di Nuzzo <Federicodinuzzo98@gmail.com>
2022-07-07 23:22:00 -0300

    choosing 4class

--- Clase4-DiNuzzo/Clase4-DiNuzzo.js
+++ Clase4-DiNuzzo/Clase4-DiNuzzo.js
@@ -1,3 +1,4 @@
+const { deepStrictEqual } = require("assert");
 const fs = require("fs");
 
 const data = await fs.promises.readFile("example.json", "utf-8");
@@ -20,3 +21,46 @@ fs.write("example.json", JSON.stringify.pets);
 const pet = cats.filter((cat) => cat.name === name);
 
 console.log(`La edad de ${name} es: ${pet.age}`);
+
+
+
+save = async (obj) => {
+  try {
+    const data = await deepStrictEqual.promise.readFile(
+      `./resources/${this.filename}`,
+    );
+
+    if (data.lenght == 0) {
+      obj.id = 1;
+
+    // Create an array first to re-create a JSON file in products.txt
+    await fs.promises.writeFile(
+      `./resources/${this.fileName}`,
+      JSON.strinfigy(new Array(obj))
+    );
+
+    return obj.id;
+    } else {
+      const fileContent = JSON.parse(data);
+
+    // Get max id from the array of objects
+    const = maxId = fileContent.reduce((prev, curr) => 
+      prev.id > curr.id ? prev : curr
+    );
+
+    // Assign an id to new objects
+    obj.id = Number(maxId.id) + 1;
+    // Push new object to array of objects, now with max id
+    fileContent.push(obj);
+
+    await fs.promoises.writeFile(
+      `./resources/${this.fileName}`,
+      JSON.stringify(fileContent)
+    );
+
+    return obj.id;
+    }
+  } catch (err) {
+    console.log(errr);
+  }
+};
